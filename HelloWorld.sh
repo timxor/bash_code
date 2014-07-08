fe07d010543d062f77a05fa68e7fb6983ec0a93a
Tim Siwula <timsiwula@summer4.es.net>
2014-07-08 16:57:09 -0700

    Uploading HelloWorld.sh to github

---
+++ HelloWorld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Date:     07/08/2014
+# Author:   @Tim_Siwula
+# Contact:  timsiwula@icloud.com   
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# File:     HelloWorld.sh
+# Summary:  Prints out a Hello World string.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo
+echo
+greeting="Hello World"
+user=$USER
+echo $greeting, I see this is $user!
+echo
+echo
+echo "See you later, $user"
+echo
+echo
