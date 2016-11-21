40a33e1b9f07c41fcb570bae68c06c24021c89af
Tim Siwula <tcsiwula@usfca.edu>
2016-11-21 13:37:07 -0800

    added chron_script.sh

---
+++ demo_scripts/chron_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+say "hey man, can you hear me now?"
+
+
+#chrontab -- chronjob
+#http://stackoverflow.com/questions/5398014/using-crontab-to-execute-script-every-minute-and-another-every-24-hours
+#http://www.techradar.com/how-to/computing/apple/terminal-101-creating-cron-jobs-1305651
+
+
+#script
+#chmod
+#env EDITOR=nano crontab -e
