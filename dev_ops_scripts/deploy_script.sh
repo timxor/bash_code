24c038caf67a252bccf6d3dbadf00e4d3214142b
Tim Siwula <tcsiwula@gmail.com>
2017-11-08 00:32:44 -0800

    [auto generated] via tim's deploy_script.sh

---
+++ dev_ops_scripts/deploy_script.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+# pwd:
+# chmod +x deploy_script.sh
+# bash deploy_script.sh
+# at here /usr/local/bin/deploy_script.sh
+# alias deploy="bash deploy_script.sh"
+
+echo "-----> starting deploy_script.sh ---->"
+git status
+git add .
+git status
+git commit -m "[auto generated] via tim's deploy_script.sh"
+git push origin master
+git status
+echo "<----- done <----"
